b7f954bb2fba38afe5a98b23e2f6260ae235c50f
seeubot <155578467+seeubot@users.noreply.github.com>
2025-09-09 10:43:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,6 +8,6 @@ class Config(object):
 
     API_HASH = os.environ.get("API_HASH", "d538c2e1a687d414f5c3dce7bf4a743c")
 
-    AUDIO_THUMBNAIL = os.environ.get("AUDIO_THUMBNAIL", "")
+    AUDIO_THUMBNAIL = os.environ.get("AUDIO_THUMBNAIL", "https://t.me/PredatorHackerzZ_bot")
 
-    VIDEO_THUMBNAIL = os.environ.get("VIDEO_THUMBNAIL", "")
+    VIDEO_THUMBNAIL = os.environ.get("VIDEO_THUMBNAIL", "https://t.me/PredatorHackerzZ_bot")
